14f182902a4090e0cfd7cad52a64b9648a2fcb36
andishehtavakoli <andishehtavakoli@gmail.com>
2021-12-23 16:19:03 -0800

    Update template telegram bot

--- src/bot.py
+++ src/bot.py
@@ -4,15 +4,36 @@ from loguru import logger
 
 bot = telebot.TeleBot(os.environ['nashenas_token'])
 
+logger.info('Start bot')
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
+	logger.info('somebody starts bot')
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, str(eval(message.text)))
 
-logger.info('Start bot')
+
 bot.infinity_polling()
 logger.info('Done') 
+
+class Bot:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
