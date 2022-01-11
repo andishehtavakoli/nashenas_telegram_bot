2d5e896b9662bf4d1789fa84e4d3298a8de73ac0
andishehtavakoli <andishehtavakoli@gmail.com>
2022-01-11 10:48:03 -0800

    Create class

--- src/bot.py
+++ src/bot.py
@@ -2,36 +2,30 @@ import telebot
 import os
 from loguru import logger
 
-bot = telebot.TeleBot(os.environ['nashenas_token'])
-
-logger.info('Start bot')
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
-	logger.info('somebody starts bot')
-
-
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, str(eval(message.text)))
-
-
-bot.infinity_polling()
-logger.info('Done') 
 
 class Bot:
+	def __init__(self):
+		self.bot = telebot.TeleBot(os.environ['nashenas_bot_Token'])
+		self.echo_all=self.bot.message_handler(func=lambda message: True)(self.echo_all)
+		self.send_welcome=self.bot.message_handler(commands=['start', 'help'])(self.send_welcome)
 
 
+	def send_welcome(self,message):
+		self.bot.reply_to(message, "Howdy, how are you doing?")
+		logger.info('somebody starts bot')
+	
+	def echo_all(self,message):
+		self.bot.reply_to(message, (message.text))
 
 
+	def run(self):
+		logger.info('Start bot')
+		self.bot.infinity_polling()
+		logger.info('Done') 	
 
 
-
-
-
-
-
+mybot=Bot()
+mybot.run()
 
 
 
